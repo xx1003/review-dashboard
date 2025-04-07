66cc314b20964f03ff1519620d337df718c5f809
xx1003 <wntnwls2002@gmail.com>
2025-04-07 09:40:57 +0900

    CREATE: app.py created

---
+++ app.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import streamlit as st
+
+st.title("Hello world from local PC")
+
+# tips = sns.load_dataset("tips")
