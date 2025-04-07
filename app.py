3074d97f522aad67c41a348152198302c9109f8d
xx1003 <wntnwls2002@gmail.com>
2025-04-07 09:46:28 +0900

    UPDATE: added barplot in app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
 st.title("Hello world from local PC")
 
-# tips = sns.load_dataset("tips")
+tips = sns.load_dataset("tips")
+
+fig, ax = plt.subplots(figsize=(10,6))
+
+sns.barplot(data=tips, x='day', y='total_bill', ax=ax)
+st.pyplot(fig)
